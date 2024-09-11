110274e215a61026e7cf9342731d45ef8b4f9d59
lolamindi <laura_mday@yahoo.co.uk>
2024-09-11 21:19:46 +0200

    add background image on main page and rectify favicon issues

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,12 @@
 
+
 export default function Home() {
   return (
     <div>
-      <h1 class ="text-pink-600 font-extrabold text-5xl">Lau's Paws</h1>
-      <h2>Licensed and registered dog boarding service operating in Ibstock, Leicestershire</h2>
+      <div class="bg-puppies h-[600px] w-6/6">
+        <h1 class="text-pink-600 font-extrabold text-5xl">Lau's Paws</h1>
+        <h2>Licensed and registered dog boarding service operating in Ibstock, Leicestershire</h2>
+      </div>
     </div>
   );
 }
