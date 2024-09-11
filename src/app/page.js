75b00ca9f49abc90b17c9001e97f39d08ae90d31
lolamindi <laura_mday@yahoo.co.uk>
2024-09-11 07:55:02 +0200

    remove default home page and css to replace with basic first page elements

---
+++ src/app/page.js
@@ -0,0 +1,9 @@
+
+export default function Home() {
+  return (
+    <div>
+      <h1 class ="text-pink-600 font-extrabold text-5xl">Lau's Paws</h1>
+      <h2>Licensed and registered dog boarding service operating in Ibstock, Leicestershire</h2>
+    </div>
+  );
+}
