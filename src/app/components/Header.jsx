e8974163f9546aa1145390930980a49dd78a5545
lolamindi <laura_mday@yahoo.co.uk>
2024-09-15 00:55:27 +0200

    start header

---
+++ src/app/components/Header.jsx
@@ -0,0 +1,8 @@
+export default function Header() {
+  return <header> 
+    <div class="bg-pink-500 h-16">
+    <h1 class="text-pink-100 font-bold text-5xl ml-16 pt-2">Lau's Paws</h1>
+    </div>
+
+  </header>;
+}
--- src/app/page.js
+++ src/app/page.js
@@ -2,11 +2,10 @@
 
 export default function Home() {
   return (
-    <div>
+    <section>
       <div class="bg-puppies h-[600px] w-6/6">
-        <h1 class="text-pink-600 font-extrabold text-5xl">Lau's Paws</h1>
-        <h2>Licensed and registered dog boarding service operating in Ibstock, Leicestershire</h2>
+        <h2 class="text-black font-regular italic flex pt-10">Licensed and registered dog boarding service operating in Ibstock, Leicestershire</h2>
       </div>
-    </div>
+    </section>
   );
 }
