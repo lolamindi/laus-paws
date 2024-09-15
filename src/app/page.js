77b3c4254395898e329f26cbe96f55c02c7d2899
lolamindi <laura_mday@yahoo.co.uk>
2024-09-16 00:07:50 +0200

    add new logo to header

--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <section>
       <div class="bg-puppies h-[600px] w-6/6">
-        <h2 class="text-black font-regular italic flex pt-10">Licensed and registered dog boarding service operating in Ibstock, Leicestershire</h2>
+        <h2 class="text-black font-regular italic flex pt-10 pl-10">Licensed and registered dog boarding service operating in Ibstock, Leicestershire</h2>
       </div>
     </section>
   );
