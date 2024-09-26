aaeb5ca6f1ff3cfa7c98d131115ce80940a14db6
lolamindi <laura_mday@yahoo.co.uk>
2024-09-26 20:50:56 +0200

    add footer

---
+++ src/app/components/Footer.jsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return <footer> 
+    <div class="bg-pink-200 h-16 flex items-center">
+    </div>
+
+  </footer>;
+}
--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Head from "next/head";
 import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,6 +21,7 @@ export default function RootLayout({ children }) {
       <Header />
       <main>{children}</main>
       </body>
+      <Footer />
     </html>
   );
 }
