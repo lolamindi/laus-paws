596e704773694286f7c3de841ea1ae6693eceede
lolamindi <laura_mday@yahoo.co.uk>
2024-09-27 22:01:36 +0200

    complete social nav bar and add it to the footer

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -1,9 +1,12 @@
+import SocialNavBar from "./SocialNavBar.jsx";
+
 export default function Footer() {
   const currentYear = new Date().getFullYear();
 
   return (
-    <footer className="bg-pink-200 h-16 flex items-center border-t-2 border-pink-500 p-4 text-gray-900">
-      © {currentYear}, Lau's Paws      
+    <footer className="bg-pink-200 h-16 flex items-center justify-between border-t-2 border-pink-500 pl-4 pr-8 text-gray-900">
+      © {currentYear}, Lau's Paws  
+      <SocialNavBar/>    
     </footer>
   );
 }
