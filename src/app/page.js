8ec6877be3eacc1438e5669231195c4004da8f20
lolamindi <laura_mday@yahoo.co.uk>
2024-10-02 23:34:49 +0200

    add dog photos and start positioning them for review page

--- src/app/page.js
+++ src/app/page.js
@@ -1,18 +1,16 @@
 export default function Home() {
   return (
     <>
-      <div className="bg-puppies h-[600px] relative">
+      <main className="bg-puppies h-[600px] relative">
         <article className="absolute inset-0 flex justify-center mt-10">
           <div className="w-[53%] h-[38%] bg-white/30 backdrop-blur-sm p-8 rounded-md flex flex-col items-center justify-center">
             <h1 className="text-4xl text-center text-pink-700 font-semibold">
               Home boarding and daycare for small to medium dogs
             </h1>
-            <button className="bg-pink-600 w-40 h-10 rounded-md mt-6 flex items-center justify-center">
-              <span className="text-white text-center text-xl">Get in touch</span>
-            </button>
+            <button className="bg-pink-600 w-40 h-10 rounded-md mt-6 flex items-center justify-center text-white text-center text-xl">Get in touch</button>
           </div>
         </article>
-      </div>
+      </main>
     </>
   );
 }
