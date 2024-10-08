4e346e143483911a9a19dc9100d8006814c70d04
lolamindi <laura_mday@yahoo.co.uk>
2024-10-08 23:31:46 +0200

    get map of dogs json to show up on the page

--- src/app/components/DogReviewCard.jsx
+++ src/app/components/DogReviewCard.jsx
@@ -1,15 +1,1 @@
-import Image from "next/image";
-import { dogs } from "../../../public/assets/dogs.json";
 
-export default function DogReviewCard({ dog }) {
-  return (
-    <article>
-      <Image
-        src= "/assets/img/review-dog1.jpg"
-        width={350}
-        height={200}
-        alt={`small dog`} 
-      />
-    </article>
-  );
-}
