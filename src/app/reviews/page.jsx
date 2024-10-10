c56722e33490fa7032a05dce5d9a27693cdf41d2
lolamindi <laura_mday@yahoo.co.uk>
2024-10-11 00:26:14 +0200

    add height to images and style review cards

--- src/app/reviews/page.jsx
+++ src/app/reviews/page.jsx
@@ -9,14 +9,14 @@ export default function Reviews({ dogs = dogData }) {
           {dogs.map((dog) => (
             <li key={dog.id} className="flex flex-col items-center rounded-lg border-2 border-pink-700 p-4 bg-white">
               <article>
-                <Image className="pb-2"
+                <Image className="pb-2 h-[243px]"
                   src={dog.image}
                   width={325}
-                  height={200}
+                  height={243}
                   alt={`small dog called ${dog.dog_name}`}
                 />
-                <span className="font-semibold text-lg pb-2">{dog.dog_name}</span>
-                <p className="w-[325px]">{dog.review}</p>
+                <span className="font-semibold text-lg">{dog.dog_name}</span>
+                <blockquote className="w-[325px] border-t-2 pt-1 border-pink-800">&ldquo;{dog.review}&rdquo;</blockquote>
               </article>
             </li>
           ))}
