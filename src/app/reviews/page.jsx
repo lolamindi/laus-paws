d9e0bba59e7930c2119ebd6b8bd52c7ebcc06390
lolamindi <laura_mday@yahoo.co.uk>
2024-10-14 00:02:26 +0200

    alter reviews page text and padding for more harmony

--- src/app/reviews/page.jsx
+++ src/app/reviews/page.jsx
@@ -6,22 +6,20 @@ export default function Reviews({ dogs = dogData }) {
     <main className="relative flex justify-center items-center min-h-screen bg-[#edd4cd]">
       <section className="absolute top-4 left-14 p-6 text-gray-800">
         <h1 className="text-2xl font-bold pb-2">Reviews</h1>
-        <p className="text-left w-[95%]">
-          Welcome to the Lau's Paws reviews page! You can read our 100% positive
-          reviews from human customers&nbsp;
+        <p className="text-left w-[97%] text-lg">
+          Welcome to the Lau's Paws reviews page! You can read the&nbsp;
           <a
             href="https://www.facebook.com/LausPawsIbstock/reviews"
             target="_blank"
             rel="noopener noreferrer"
-            className="text-pink-800 underline"
+            className="text-pink-800"
           >
-            on our Facebook page
+           100% positive recommendations&nbsp;
           </a>
-          , but here are some tail-wagging testimonials from our favorite
-          canine visitors:
+          our human customers have kindly left for us on Facebook, but here are some tail-wagging testimonials from our favourite canine visitors:
         </p>
       </section>
-      <section className="grid grid-cols-3 gap-x-16 gap-y-8 mt-36 mb-20">
+      <section className="grid grid-cols-3 gap-x-16 gap-y-8 mt-40 mb-20">
         <ul className="contents">
           {dogs.map((dog) => (
             <li
